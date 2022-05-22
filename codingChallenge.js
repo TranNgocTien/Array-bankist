1717194888169ba25a11672706a8f2b5eb3e3939
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-23 06:41:42 +0700

    Find the most frequent item of an array

--- codingChallenge.js
+++ codingChallenge.js
@@ -8,3 +8,27 @@ const ascending= function(arr){
 }
 console.log(ascending(arr1));
 
+//8. Write a JavaScript program to find the most frequent item of an array. 
+//Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+//Sample Output : a ( 5 times )
+
+let arr2=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+let m=0;
+let mf=1;
+let item=0;
+for(let i=0;i<arr2.length;i++){
+    for(let j=i;j<arr2.length;j++){
+        if(arr2[i]===arr2[j]){
+            m++;
+        }
+        if(m>mf){
+            mf=m;
+            item=arr2[j];
+        }
+    }
+    m=0;
+}
+console.log( `${item} ( ${mf} times )`);
+
+
+
