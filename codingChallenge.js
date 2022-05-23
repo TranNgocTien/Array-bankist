e88c27bd81de770d0ac6005b4f08982bf70a951f
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-05-23 19:17:26 +0700

    print array of array

--- codingChallenge.js
+++ codingChallenge.js
@@ -54,3 +54,24 @@ const swapCaseStr=function(str){
 }
 swapCaseStr('The Quick Brown Fox');
 console.log(result.join(''));
+
+// 10. Write a JavaScript program which prints the elements of the following array. Go to the editor
+// Note : Use nested for loops.
+// Sample array : var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+// Sample Output :
+// "row 0"
+// " 1"
+// " 2"
+// " 1"
+// " 24"
+// "row 1"
+let a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+const arrOfArrs=function(arrs){
+    for(const [i,el] of arrs.entries()){
+        console.log(`row ${i}`);
+        for(const arr of el){
+            console.log(arr);
+        }
+    }
+}
+arrOfArrs(a);
